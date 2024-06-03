bb751fec136ac9560f171ccb4b7212a792ed9735
D'Bonis <dev@rodolfodebonis.com.br>
2024-06-03 20:10:30 -0300

    feat: add hook for move project to root folder

---
+++ hooks/post_gen_project.py
@@ -0,0 +1,14 @@
+import os
+import shutil
+
+dir_name = '{{cookiecutter.package_name}}'
+
+if os.path.isdir(dir_name):
+    for filename in os.listdir(dir_name):
+        full_path = os.path.join(dir_name, filename)
+
+        shutil.move(full_path, '.')
+
+    os.rmdir(dir_name)
+else:
+    print(f'Diretório {dir_name} não encontrado')
